5e37137f5a06783632159af97edfb80fd199756c
Milo Bauman <mjb01161@uga.edu>
2025-06-04 14:45:40 -0400

    Added better example

--- src/example1.py
+++ src/example1.py
@@ -1,2 +1,15 @@
 import gymnasium as gym
-env = gym.make('CartPole-v1')
+
+# Initialize an environment. Render mode determines how the environment is visualized. 
+env = gym.make("LunarLander-v3", render_mode="human")
+
+# Resetting will get the 1st observation of the environment as well as additional info. 
+observation, info = env.reset()
+
+episode_over = False
+while not episode_over:
+    action = env.action_space.sample()  # agent policy that uses the observation and info
+    observation, reward, terminated, truncated, info = env.step(action)
+    episode_over = terminated or truncated
+
+env.close()
