ebe9c3387a42ca55dfadc3f5c84b0a5014fa0820
Milo Bauman <mjb01161@uga.edu>
2025-07-01 11:19:11 -0400

    Added new example

--- src/rl-lib/RLlibex2.py
+++ src/rl-lib/RLlibex2.py
@@ -1,8 +1,24 @@
-from ray import tune
+from ray.rllib.algorithms.ppo import PPOConfig
+import gymnasium as gym
 
-tune.run("PPO", 
-         config = {"env": "CartPole-v1", 
-                   # other configurations go here
-                   # if none supplied, default will be used
-                   }
-            )
+config = (
+    PPOConfig()
+    .environment(env="CartPoleV1")
+    .rollouts(num_rollour_workers=0)
+)
+trainer = config.build()
+
+# Training loop
+for i in range(10): 
+    result = trainer.train()
+    print(f"Iteration {i}: reward = {result['episode_reward_mean']:.2f}")
+
+env = gym.make("CartPole-v1", render_mode="human")
+obs, _ = env.reset()
+done = False
+
+while not done:
+    action = trainer.compute_single_action(obs)
+    obs, reward, terminated, truncated, _ = env.step(action)
+    done = terminated or truncated
+env.close()
